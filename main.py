b2ca0ed18c302c2ec429f3a79591f6be5360e87e
Amy <amywaldrop@mail.weber.edu>
2025-04-24 21:23:27 -0600

    Edit: I guess this will do

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from dog import Dog
 
 if __name__ == "__main__":
     # TODO: Create an instance of the Animal class
-    animal1 = Animal("Leo", "Lion")
+    animal1 = Animal("Big Cat", "Lion")
     # TODO: Print the Animal instance
     print(animal1)
     # TODO: Call the method to make a generic animal sound
     print(animal1.speak()) 
     # TODO: Create an instance of the Dog class
-    dog1 = Dog("Buddy", "Canine", "Golden Retriever")
+    dog1 = Dog("Bloo", "Canine", "Mutt")
     # TODO: Print the Dog instance
     print(dog1)
     # TODO: Call the method to make the dog-specific sound
