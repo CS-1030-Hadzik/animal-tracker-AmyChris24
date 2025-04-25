9007f2a85316cd8a3636620afdb7f0892a2baf4d
Amy <amywaldrop@mail.weber.edu>
2025-04-24 21:01:06 -0600

    EDIT: Fixing issues

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from dog import Dog
 
 if __name__ == "__main__":
     # TODO: Create an instance of the Animal class
-    animal = Animal("Leo", "Lion")
+    animal1 = Animal("Leo", "Lion")
     # TODO: Print the Animal instance
-    print(animal)
+    print(Animal)
     # TODO: Call the method to make a generic animal sound
-    print(animal.speak())
+    print(Animal.speak()) 
     # TODO: Create an instance of the Dog class
-    dog = Dog("Buddy", "Canine", "Golden Retriever")
+    dog1 = Dog("Buddy", "Canine", "Golden Retriever")
     # TODO: Print the Dog instance
-    print(dog)
+    print(Dog)
     # TODO: Call the method to make the dog-specific sound
-    print(dog.speak())
+    print(Dog.speak())
     # TODO print out all the animals
     print("All Animals:")
-    for a in Animal.all_animals:
-        print(a)
+    print("All Animals:")
+    for animal in Animal.all_animals:
+        print(animal)
+
 
